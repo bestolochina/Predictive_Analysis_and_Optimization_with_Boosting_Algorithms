fc9a594b170a2873396c95ee4645bee05f9cdf0c
bestolochina <bestolochina@gmail.com>
2025-05-05 22:15:36 +0700

    Completed stage 1

--- Predictive Analysis and Optimization with Boosting Algorithms/task/boost.py
+++ Predictive Analysis and Optimization with Boosting Algorithms/task/boost.py
@@ -3,16 +3,27 @@ import requests
 import pandas as pd
 
 
-def download_data():
-    # Create data directory
-    if not os.path.exists('../data'):
-        os.mkdir('../data')
+def download_data() -> pd.DataFrame:
+    data_dir = '../data'
+    file_name = 'insurance.csv'
+    file_path = os.path.join(data_dir, file_name)
 
-    # Download data if it is unavailable
-    if 'insurance.csv' not in os.listdir('../data'):
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+
+    if not os.path.exists(file_path):
         url = "https://www.dropbox.com/scl/fi/r5033u0e89bpjrk3n9snx/insurance.csv?rlkey=8sv6cnesc6kkqmu6jrizvn9ux&dl=1"
-        r = requests.get(url, allow_redirects=True)
-        open('../data/insurance.csv', 'wb').write(r.content)
+        response = requests.get(url, allow_redirects=True)
+        response.raise_for_status()  # Raise error if the download failed
+        with open(file_path, 'wb') as f:
+            f.write(response.content)
 
+    return pd.read_csv(file_path)
 
-# Your solution starts here
+if __name__ == '__main__':
+    df = download_data().drop_duplicates()
+
+    numerical_features = df.select_dtypes(include=['float64', 'int64']).columns.tolist()
+    categorical_features = df.select_dtypes(include=['object']).columns.tolist()
+    features = {'numerical': numerical_features, 'categorical': categorical_features, 'shape': list(df.shape)}
+    print(features)
